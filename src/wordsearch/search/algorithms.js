e5e0b3c5e6c1b15c0e6d2650f51b9eb91c75c3a8
Martin Charles <martincharles07@gmail.com>
2017-10-07 20:47:50 -0400

    Added Prefix Solver Algorithm
    The performance seems to be similar. This might be because I'm using
    babel-node or because the puzzles aren't hard enough or because the word
    lists aren't long enough.

--- src/wordsearch/search/algorithms.js
+++ src/wordsearch/search/algorithms.js
@@ -1,5 +1,7 @@
 import { findMatches as findMatchesBruteForce } from './force';
+import { findMatches as findMatchesPrefixTable } from './prefix';
 
 export default [
   { name: 'brute force', f: findMatchesBruteForce },
+  { name: 'prefix table', f: findMatchesPrefixTable },
 ];
--- src/wordsearch/search/search.test.js
+++ src/wordsearch/search/search.test.js
@@ -1,4 +1,4 @@
-import { solve } from '../';
+import { solve, Directions } from '../';
 import algorithms from './algorithms';
 import puzzles from '../data';
 
@@ -7,7 +7,7 @@ algorithms.forEach(({ name, f: findMatches }) => {
     Object.entries(puzzles)
       .forEach(([name, { words, rows } ]) =>
         it(`should solve the ${name} puzzle`, () => {
-          const { matches, grid } = solve(rows, words);
+          const { matches } = solve(rows, words, Directions, findMatches);
 
           expect(Object.keys(matches)).toHaveLength(words.length);
         })
