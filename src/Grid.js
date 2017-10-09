baec3215b4687d2d2c530f9b6ef34690c3d8c25f
Martin Charles <martincharles07@gmail.com>
2017-10-08 20:44:49 -0400

    Elegant Code
    This code was written back before I realized map had an index param in
    the callback.

--- src/Grid.js
+++ src/Grid.js
@@ -13,27 +13,25 @@ export default class Grid extends PureComponent {
       return null;
     }
 
-    // collect selctions
-    let id = 0;
-    const gridSelectedNodes = selected.map(selection =>
-      <GridSelection
-        key={`match${id++}`}
-        grid={grid}
-        selection={selection}
-        onUnselect={onUnselect} />
-    );
-
     return (
       <div
         className='Grid'
-        style={{gridTemplateColumns: `repeat(${grid.columns()}, minmax(1em, 2em))`}}>
+        style={{
+          gridTemplateColumns: `repeat(${grid.columns()}, minmax(1em, 2em))`
+        }}>
 
         <GridNodes
           grid={grid}
           extras={nodeExtras}
           onSelect={onSelect} />
 
-        { gridSelectedNodes }
+        { selected.map((selection, i) =>
+          <GridSelection
+            key={`match${i}`}
+            grid={grid}
+            selection={selection}
+            onUnselect={onUnselect} />
+        ) }
       </div>
     );
   }
