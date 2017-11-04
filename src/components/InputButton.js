5159e2ce489ec08ad322aef34d4923d0c6241516
Martin Charles <martincharles07@gmail.com>
2017-11-04 14:07:19 -0400

    Reviewed PR
    Print style sheets are broken because of the new two pane view so just
    ignore them all together.

--- src/components/InputButton.js
+++ src/components/InputButton.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { inputButton, button } from './Button.css';
 
-const ButtonInput = ({ children, className, onChange }) => {
-  return (
-    <label
-      className={[className, inputButton, button].join(' ')}>
-      { children }
+const ButtonInput = ({ children, className, onChange }) =>
+  <label
+    className={[className, inputButton, button].join(' ')}>
+    { children }
 
-      <input
-        type='file'
-        onChange={onChange} />
-    </label>
-  );
-}
+    <input
+      type='file'
+      onChange={onChange} />
+  </label>
 
 export default ButtonInput;
