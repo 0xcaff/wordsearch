2c89bcb0ddbc28c1a2761dc69325a21252de38d7
Martin Charles <martincharles07@gmail.com>
2017-11-04 13:52:55 -0400

    Separate CSS From JavaScript in Output
    Fixes #43

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -272,6 +272,7 @@ module.exports = {
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin({
       filename: cssFilename,
+      allChunks: true,
     }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
