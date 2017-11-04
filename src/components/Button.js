069e83b515c65bb79a1c47247c705f3b0d98b10e
Martin Charles <martincharles07@gmail.com>
2017-11-04 13:40:04 -0400

    Simplified Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import { button } from './Button.css';
 
-const Button = (props) => {
-  // TODO: Remove Me and instead make props explicit
-  const filteredProps = Object.entries(props)
-    .filter(([ key, value ]) => key !== 'className')
-    .reduce((acc, [ key, value ]) => Object.assign(acc, { [key]: value }), {});
-
-  return (
-    <span
-      className={[ button, props.className ].join(' ')}
-      {...filteredProps}>
-      { props.children }
-    </span>
-  );
-}
+const Button = ({ onClick, className, children }) =>
+  <span
+    className={[ button, className ].join(' ')}
+    onClick={onClick}>
+    { children }
+  </span>
 
 export default Button;
