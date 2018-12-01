e8acc64e455234db426f26d6161200ac7274e50f
Martin Charles <martincharles07@gmail.com>
2018-12-01 11:28:41 -0500

    Create Image Annotation Fetcher

---
+++ src/components/ImageAnnotationFetcher.tsx
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import { Symbol } from "../utils/google-cloud-vision-type";
+import { Base64EncodeTransformer, collect, fileAsStream } from "../utils/files";
+import { ProgressTransformer } from "../utils/stream";
+import { getImageAnnotations } from "../utils/google-cloud-vision";
+
+interface Props {
+  file: File;
+  children: (props: ChildProps) => React.ReactNode;
+}
+
+interface ChildProps {
+  annotations: Symbol[];
+}
+
+enum LoadingStateType {
+  STARTING,
+  ENCODING_FILE,
+  API_REQUEST,
+  DONE
+}
+
+type LoadingState =
+  | { type: LoadingStateType.STARTING }
+  | {
+      type: LoadingStateType.ENCODING_FILE;
+
+      /**
+       * A number between 0 and 100 indicating how much of the encoding step is done.
+       */
+      progress: number;
+    }
+  | { type: LoadingStateType.API_REQUEST }
+  | { type: LoadingStateType.DONE; result: Symbol[] };
+
+interface State {
+  loading: LoadingState;
+}
+
+/**
+ * Fetches annotations for a file. Displays loading states. Doesn't re-fetch on prop change.
+ */
+class ImageAnnotationFetcher extends Component<Props, State> {
+  state = {
+    loading: { type: LoadingStateType.STARTING } as LoadingState
+  };
+
+  async componentDidMount(): Promise<void> {
+    this.setState({
+      loading: { type: LoadingStateType.ENCODING_FILE, progress: 0 }
+    });
+
+    const encoded = await collect(
+      fileAsStream(this.props.file)
+        .pipeThrough(
+          new ProgressTransformer(progress =>
+            this.setState({
+              loading: {
+                type: LoadingStateType.ENCODING_FILE,
+                progress: Math.round((progress / this.props.file.size) * 100)
+              }
+            })
+          )
+        )
+        .pipeThrough(new Base64EncodeTransformer())
+    );
+
+    this.setState({ loading: { type: LoadingStateType.API_REQUEST } });
+    const symbols = await getImageAnnotations(encoded);
+    this.setState({
+      loading: { type: LoadingStateType.DONE, result: symbols }
+    });
+  }
+
+  render(): React.ReactNode {
+    const loading = this.state.loading;
+    switch (loading.type) {
+      case LoadingStateType.STARTING:
+        return "Loading...";
+
+      case LoadingStateType.ENCODING_FILE:
+        return `Encoding File ${loading.progress}`;
+
+      case LoadingStateType.API_REQUEST:
+        return "Asking The Robots for Help...";
+
+      case LoadingStateType.DONE:
+        return this.props.children({ annotations: loading.result });
+    }
+  }
+}
+
+export default ImageAnnotationFetcher;
--- src/routes/ImageInput.tsx
+++ src/routes/ImageInput.tsx
@@ -1,41 +1,14 @@
-import React, { Component } from "react";
-import { Base64EncodeTransformer, collect, fileAsStream } from "../utils/files";
-import { ProgressTransformer } from "../utils/stream";
-import { getImageAnnotations } from "../utils/google-cloud-vision";
+import React from "react";
+import ImageAnnotationFetcher from "../components/ImageAnnotationFetcher";
 
 interface Props {
   file: File;
 }
 
-interface State {
-  progress: number;
-}
-
-class ImageInput extends Component<Props, State> {
-  state = {
-    progress: 0
-  };
-
-  async componentDidMount(): Promise<void> {
-    const encoded = await collect(
-      fileAsStream(this.props.file)
-        .pipeThrough(
-          new ProgressTransformer(progress => this.setState({ progress }))
-        )
-        .pipeThrough(new Base64EncodeTransformer())
-    );
-
-    const symbols = await getImageAnnotations(encoded);
-    console.log(symbols);
-  }
-
-  render() {
-    const progress = Math.round(
-      (this.state.progress / this.props.file.size) * 100
-    );
-
-    return <div>{progress}</div>;
-  }
-}
+const ImageInput = (props: Props) => (
+  <ImageAnnotationFetcher file={props.file}>
+    {childProps => JSON.stringify(childProps.annotations)}
+  </ImageAnnotationFetcher>
+);
 
 export default ImageInput;
