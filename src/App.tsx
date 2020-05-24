85dd584107afe1571e02755d6d8a929566afd1ca
Martin Charles <martincharles07@gmail.com>
2020-05-23 22:49:43 -0700

    Convert App to FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Suspense, lazy } from "react";
+import React, { Suspense, lazy } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -12,58 +12,52 @@ const InputSelection = lazy(() => import("./routes/InputSelection"));
 const TextInput = lazy(() => import("./routes/TextInput"));
 const ViewPuzzleWithData = lazy(() => import("./routes/ViewPuzzle"));
 
-class App extends Component {
-  render() {
-    return (
-      <Suspense fallback={<FullPageLoading />}>
-        <Router>
-          <div className="App">
-            <Switch>
-              <Route path="/" render={() => <InputSelection />} exact />
+const App = () => (
+  <Suspense fallback={<FullPageLoading />}>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" render={() => <InputSelection />} exact />
 
-              <Route
-                path="/input/text"
-                render={(props) => (
-                  <TextInput
-                    startingRows={
-                      props.location.state && props.location.state.rows
-                    }
-                    startingWords={
-                      props.location.state && props.location.state.words
-                    }
-                    solvePuzzle={(rows, words) =>
-                      props.history.push("/view", { rows, words })
-                    }
-                  />
-                )}
-                exact
+          <Route
+            path="/input/text"
+            render={(props) => (
+              <TextInput
+                startingRows={props.location.state && props.location.state.rows}
+                startingWords={
+                  props.location.state && props.location.state.words
+                }
+                solvePuzzle={(rows, words) =>
+                  props.history.push("/view", { rows, words })
+                }
               />
+            )}
+            exact
+          />
 
-              <Route
-                path="/view/:id?"
-                render={(props) => (
-                  <ViewPuzzleWithData
-                    id={props.match.params?.id}
-                    puzzleData={props.location.state}
-                    viewPuzzle={(puzzleId) =>
-                      props.history.replace(`/view/${puzzleId}`)
-                    }
-                    toEditorWithPuzzle={(puzzle) =>
-                      props.history.push("/input/text", puzzle)
-                    }
-                  />
-                )}
-                exact
+          <Route
+            path="/view/:id?"
+            render={(props) => (
+              <ViewPuzzleWithData
+                id={props.match.params?.id}
+                puzzleData={props.location.state}
+                viewPuzzle={(puzzleId) =>
+                  props.history.replace(`/view/${puzzleId}`)
+                }
+                toEditorWithPuzzle={(puzzle) =>
+                  props.history.push("/input/text", puzzle)
+                }
               />
+            )}
+            exact
+          />
 
-              {/* Redirect any non-matching routes to the exact root. */}
-              <Redirect from="/" to="/" />
-            </Switch>
-          </div>
-        </Router>
-      </Suspense>
-    );
-  }
-}
+          {/* Redirect any non-matching routes to the exact root. */}
+          <Redirect from="/" to="/" />
+        </Switch>
+      </div>
+    </Router>
+  </Suspense>
+);
 
 export default App;
