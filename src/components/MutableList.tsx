fe25bc567c984714a94abaa0a86a51298bd7c686
Martin Charles <martincharles07@gmail.com>
2020-05-23 22:46:43 -0700

    Convert MutableList to Hooks

--- src/components/MutableList.tsx
+++ src/components/MutableList.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useCallback } from "react";
 import styles from "./List.module.css";
 
 interface Props {
@@ -7,70 +7,78 @@ interface Props {
   onChange: (newItems: string[]) => void;
 }
 
-class MutableList extends Component<Props> {
-  private removeItem = (idx: number) => {
-    const items = this.props.items;
-    const newItems = [...items.slice(0, idx), ...items.slice(idx + 1)];
-    this.props.onChange(newItems);
-  };
+const MutableList = ({ items, onChange, children }: Props) => {
+  const removeItem = useCallback(
+    (idx: number) => {
+      const newItems = [...items.slice(0, idx), ...items.slice(idx + 1)];
+      onChange(newItems);
+    },
+    [items, onChange]
+  );
 
-  private addItems = (...items: string[]) =>
-    this.props.onChange(this.props.items.concat(items));
+  const addItems = useCallback(
+    (...newItems: string[]) => onChange(items.concat(newItems)),
+    [items, onChange]
+  );
 
-  private handlePaste = (event: React.ClipboardEvent<HTMLInputElement>) => {
-    event.preventDefault();
+  const handlePaste = useCallback(
+    (event: React.ClipboardEvent<HTMLInputElement>) => {
+      event.preventDefault();
 
-    const lines = event.clipboardData.getData("text/plain").split(/\r?\n/);
-    this.addItems(...lines);
-  };
+      const lines = event.clipboardData.getData("text/plain").split(/\r?\n/);
+      addItems(...lines);
+    },
+    [addItems]
+  );
 
-  private handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key !== "Enter") {
-      // ignore everything except enter key without shift
-      return;
-    }
+  const handleKeyPress = useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key !== "Enter") {
+        // ignore everything except enter key without shift
+        return;
+      }
 
-    const value = event.currentTarget.value;
-    if (value === "") {
-      return;
-    }
+      const value = event.currentTarget.value;
+      if (value === "") {
+        return;
+      }
 
-    event.currentTarget.value = "";
-    this.addItems(value);
-  };
+      event.currentTarget.value = "";
+      addItems(value);
+    },
+    [addItems]
+  );
 
-  render() {
-    return (
-      <div className={styles.list}>
-        <ul>
-          {this.props.items.map((item, idx) => (
-            <li key={idx} className={styles.listItem}>
-              <span className={styles.listItemContent}>
-                {this.props.children(item)}
+  return (
+    <div className={styles.list}>
+      <ul>
+        {items.map((item, idx) => (
+          <li key={idx} className={styles.listItem}>
+            <span className={styles.listItemContent}>
+              {children(item)}
 
-                <span
-                  onClick={() => this.removeItem(idx)}
-                  className={styles.removeButton}
-                >
-                  x
-                </span>
+              <span
+                onClick={() => removeItem(idx)}
+                className={styles.removeButton}
+              >
+                x
               </span>
-            </li>
-          ))}
-
-          <li className={styles.listItem} key={-1} title="Enter Word...">
-            <input
-              className={styles.listItemInput}
-              onPaste={this.handlePaste}
-              onKeyPress={this.handleKeyPress}
-              type="Text"
-              placeholder="Enter Word..."
-            />
+            </span>
           </li>
-        </ul>
-      </div>
-    );
-  }
-}
+        ))}
+
+        <li className={styles.listItem} key={-1} title="Enter Word...">
+          <input
+            className={styles.listItemInput}
+            onPaste={handlePaste}
+            onKeyPress={handleKeyPress}
+            type="Text"
+            placeholder="Enter Word..."
+          />
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default MutableList;
